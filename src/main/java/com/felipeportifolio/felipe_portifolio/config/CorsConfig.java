5133d3c5a931be727e1a3446d53e9e173d57440a
Felipe Silva <joaofelipems21@gmail.com>
2024-12-05 10:48:42 -0300

    Update CORS and Configuration of HikariCP

--- src/main/java/com/felipeportifolio/felipe_portifolio/config/CorsConfig.java
+++ src/main/java/com/felipeportifolio/felipe_portifolio/config/CorsConfig.java
@@ -7,15 +7,18 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class CorsConfig {
-	
-	@Bean
-	public WebMvcConfigurer corsConfigurer() {
-		return new WebMvcConfigurer() {
-			@Override
-			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**").allowedOrigins("http://localhost:4200", "https://felipedev.familiaweb.com.br", "https://felipeportifoliosite-hwmwru2w4-felipemt21s-projects.vercel.app");
-			}
-		};
-	}
 
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:4200", "https://felipedev.familiaweb.com.br", "https://felipeportifoliosite-hwmwru2w4-felipemt21s-projects.vercel.app")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
 }
